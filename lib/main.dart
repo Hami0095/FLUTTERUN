6c53504419f37d2d24c5df3eb0d15d34db8085e9
Hami0095 <abdman0095@gmail.com>
2022-10-17 22:03:04 +0500

    build started

--- lib/main.dart
+++ lib/main.dart
@@ -6,52 +6,10 @@ import 'package:flame/flame.dart';
 import 'package:rive/rive.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
-  runApp(const MyGame());
-}
-
-class MyGame extends StatelessWidget {
-  const MyGame({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Run!',
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late FlameGame game;
-
-  @override
-  void initState() {
-    super.initState();
-    game = FlameGame();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text("Run!"),
-      ),
-      body: const Center(
-        child: RiveAnimation.network(
-          'https://cdn.rive.app/animations/vehicles.riv',
-        ),
-      ),
-    );
-  }
+  final game = FlameGame();
+  runApp(
+    GameWidget(
+      game: game,
+    ),
+  );
 }
