5673a54c3d6b4a76856c9da0a5b094cce8e14da1
Hami0095 <abdman0095@gmail.com>
2022-10-19 20:05:30 +0500

    enemy Component added

--- lib/resources/enemy.dart
+++ lib/resources/enemy.dart
@@ -1,40 +1,48 @@
 import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:run/resources/game_world.dart';
 
-class GamePlayer extends FlameGame {
-  String player;
+class GameEnemy extends FlameGame {
+  String enemy;
   Vector2 worldSize;
-  GamePlayer({required this.player, required this.worldSize});
+  GameEnemy({required this.enemy, required this.worldSize});
   @override
   Future<void> onLoad() async {
     Artboard characterArtboard = await loadArtboard(
-      RiveFile.asset(player),
+      RiveFile.asset(enemy),
     );
-    PlayerComponent characterComponent = PlayerComponent(
+    EnemyComponent characterComponent = EnemyComponent(
       playerArtboard: characterArtboard,
     );
-    characterComponent.position = worldSize * 0.64;
-    var characterController = OneShotAnimation('Running', autoplay: true);
+    characterComponent.position = worldSize * 0.615;
+    var characterController = OneShotAnimation('Animation 1', autoplay: true);
     characterArtboard.addController(characterController);
     add(characterComponent);
     return super.onLoad();
   }
 }
 
-class PlayerComponent extends RiveComponent with HasGameRef {
+class EnemyComponent extends RiveComponent with HasGameRef {
   Artboard playerArtboard;
-  PlayerComponent({required this.playerArtboard})
+  EnemyComponent({required this.playerArtboard})
       : super(
           artboard: playerArtboard,
           size: Vector2.all(100),
         );
   @override
   Future<void>? onLoad() {
-    position.x = 0;
+    position.x = 50;
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    x = x;
+    debugPrint("$x");
   }
 }
