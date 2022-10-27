dee64f9836561a28fddf2bf4de177f167a7d0c78
Hami0095 <abdman0095@gmail.com>
2022-10-27 13:51:25 +0500

    MEGA CHANGE
    Rive Components were changed with SpriteComponents and collisions added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 // ignore_for_file: depend_on_referenced_packages
-
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 
 import '/game.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   MyGame game = MyGame();
   runApp(
-    GameWidget(
-      game: game,
+    MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: GameWidget(
+            game: game,
+          ),
+        ),
+      ),
     ),
   );
 }
